50cfd30eac1b518941ce798f7fb40fd4274e3196
waridh <waridh@proton.me>
2024-07-30 20:36:47 -0400

    Parser can now parse let statements

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/waridh/go-monkey-interpreter/ast"
 	"github.com/waridh/go-monkey-interpreter/lexer"
 	"github.com/waridh/go-monkey-interpreter/token"
@@ -9,11 +11,12 @@ import (
 type Parser struct {
 	l         *lexer.Lexer
 	curToken  token.Token
+	errors    []string
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{l: l, errors: []string{}}
 
 	// Step the lexer properly, and fill up the parser
 	p.nextToken()
@@ -27,6 +30,74 @@ func (p *Parser) nextToken() {
 	p.peekToken = p.l.NextToken()
 }
 
+// ParseProgram is the method that will return an AST from the input lexer
+//
+// TODO: Iteration occuring here is not very clean
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) isCurToken(s token.TokenType) bool {
+	return p.curToken.Type == s
+}
+
+func (p *Parser) isPeekToken(s token.TokenType) bool {
+	return p.peekToken.Type == s
+}
+
+func (p *Parser) peekStep(s token.TokenType) bool {
+	r := p.isPeekToken(s)
+	if r {
+		p.nextToken()
+	} else {
+		msg := fmt.Sprintf("Parser expected %s but got %s", s, p.peekToken.Type)
+		p.errors = append(p.errors, msg)
+	}
+	return r
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.peekStep(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.peekStep(token.ASSIGN) {
+		return nil
+	}
+
+	// This is where expression parsing would happen
+	// TODO: Implement the next expression ast node
+	for !p.peekStep(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,1 +1,85 @@
 package parser
+
+import (
+	"testing"
+
+	"github.com/waridh/go-monkey-interpreter/ast"
+	"github.com/waridh/go-monkey-interpreter/lexer"
+)
+
+type Test struct {
+	expectedIdentifier string
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	es := p.Errors()
+
+	if len(es) == 0 {
+		return
+	}
+
+	t.Errorf("Parser has %d errors", len(es))
+	for _, msg := range es {
+		t.Errorf("parser error msg: %s", msg)
+	}
+	t.FailNow()
+}
+
+func TestLetStatements(t *testing.T) {
+	input := `
+  let x = 5;
+  let y = 10;
+  let foobar = 838383;
+  `
+
+	expected := []Test{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+	if program == nil {
+		t.Fatalf("ParseProgram returned nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("ParseProgram did not return expected number of items. Expected 3, got %d", len(program.Statements))
+	}
+
+	for i, tt := range expected {
+		stmt := program.Statements[i]
+		if !testLetStatements(t, tt.expectedIdentifier, stmt) {
+			return
+		}
+	}
+}
+
+func testLetStatements(t *testing.T, expected string, s ast.Statement) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("TokenLiteral of let statement != let")
+		return false
+	}
+
+	letStmt, ok := s.(*ast.LetStatement)
+
+	if !ok {
+		t.Errorf("s not *ast.LetStatement. got=%T", s)
+		return false
+	}
+
+	if letStmt.Name.Value != expected {
+		t.Errorf("letStmt.Name.Value != %s, got %s", expected, letStmt.Name.Value)
+		return false
+	}
+
+	if letStmt.Name.TokenLiteral() != expected {
+		t.Errorf("s.Name != %s, got %s", expected, letStmt.Name)
+		return false
+	}
+
+	return true
+}
