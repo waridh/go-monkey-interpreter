f21c179703da8a83b5c95f593494af13cb71b716
waridh <waridh@proton.me>
2024-08-02 19:22:54 -0400

    added the basic return statement without expression

--- parser/parser.go
+++ parser/parser.go
@@ -75,6 +75,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -99,5 +101,17 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		p.nextToken()
 	}
 
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	// TODO: Implement the expression node type
+	for !p.isPeekToken(token.SEMICOLON) {
+		p.nextToken()
+	}
 	return stmt
 }
