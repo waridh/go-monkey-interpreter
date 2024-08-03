9bf6a5ad777775e31c56f3c1dd04c987ef87ef46
waridh <waridh@proton.me>
2024-08-02 21:33:10 -0400

    added string representation to the ast

--- ast/ast.go
+++ ast/ast.go
@@ -1,11 +1,16 @@
 package ast
 
-import "github.com/waridh/go-monkey-interpreter/token"
+import (
+	"bytes"
+
+	"github.com/waridh/go-monkey-interpreter/token"
+)
 
 // Node is the base interface for all the nodes of the abstract syntax tree
 // in our system
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 // Statement is the interface that lets us identify if a struct is a statement
@@ -33,6 +38,14 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
 type LetStatement struct {
 	Token token.Token // For the LET token
 	Name  *Identifier
@@ -42,6 +55,18 @@ type LetStatement struct {
 func (ls *LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
+}
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
 }
 
 type Identifier struct {
@@ -54,12 +79,45 @@ func (id *Identifier) TokenLiteral() string {
 	return id.Token.Literal
 }
 
+func (id *Identifier) String() string {
+	return id.Value
+}
+
 type ReturnStatement struct {
-	Token token.Token
-	Value Expression
+	Token       token.Token
+	ReturnValue Expression
 }
 
 func (rs *ReturnStatement) statementNode() {}
 func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
 }
---
+++ ast/ast_test.go
@@ -0,0 +1,30 @@
+package ast
+
+import (
+	"testing"
+
+	"github.com/waridh/go-monkey-interpreter/token"
+)
+
+// TestString is testing the following monkey statement
+// let myVar = anotherVar;
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+	if program.String() != "let myVar = anotherVar;" {
+		t.Errorf("program.String() wrong, got %q", program.String())
+	}
+}
